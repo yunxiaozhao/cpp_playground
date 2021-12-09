20cf3d2461c3d3e88784fcfe75fa3bf99d7dd66d
赵云潇 <zhao.yunxiao@outlook.com>
2021-12-09 18:25:44 +0800

    .

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,21 +1,16 @@
 #include <iostream>
 
+class C
+{
+public:
+    int i;
+};
+
 int main()
 {
     int a = 1;
-    int &b = a;
-    int &c = b;
-    int *p = &a;
-    double d = 3.;
-    int *k = &a, **p1 = &k;
-    std::cout << **p1;
-    const int ci = 1;
-    const int &cs = ci;
-    int const k1 = 3;
-    int const *const *const kk = &p;
-    std::cout << **kk;
-    using kkkk = int *;
-    kkkk aaa = &a;
-    int constexpr ka11 = 1;
-    std::cout << *aaa << ka11;
+    int b = 2;
+    C c{};
+    std::cout << c.i;
+    return 0;
 }
